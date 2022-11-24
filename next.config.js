75d536b35cacce8fb75584c220ab3bc1f71e07a1
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-25 02:20:41 +0900

    Chore: sass set

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+	output: 'standalone',
+};
+
+module.exports = nextConfig;
