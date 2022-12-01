da7097ee37a5de8b3a0e46b8d183a82dabfd668b
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-01 22:00:41 +0900

    Feat: report page2 & page3 skeleton published

--- components/Report/ReportPage2.tsx
+++ components/Report/ReportPage2.tsx
@@ -1,3 +1,27 @@
+import styled from 'styled-components';
+
 export default function ReportPage2() {
-	return <div id='page2'>Page2</div>;
+	return (
+		<Page2Container id='page2'>
+			<NavyTop src='/assets/page2_남색상단.png' />
+			<GrayBottom src='/assets/page2_회색하단.png' />
+		</Page2Container>
+	);
 }
+
+const Page2Container = styled.div``;
+
+const Background = styled.img`
+	width: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 3;
+`;
+
+const NavyTop = styled(Background)``;
+
+const GrayBottom = styled(Background)`
+	top: unset;
+	bottom: -0.2rem;
+`;
--- components/Report/ReportPage3.tsx
+++ components/Report/ReportPage3.tsx
@@ -1,3 +1,27 @@
+import styled from 'styled-components';
+
 export default function ReportPage3() {
-	return <div id='page3'>Page3</div>;
+	return (
+		<Page3Container id='page3'>
+			<NavyTop src='/assets/page2_남색상단.png' />
+			<GrayBottom src='/assets/page2_회색하단.png' />
+		</Page3Container>
+	);
 }
+
+const Page3Container = styled.div``;
+
+const Background = styled.img`
+	width: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 3;
+`;
+
+const NavyTop = styled(Background)``;
+
+const GrayBottom = styled(Background)`
+	top: unset;
+	bottom: -0.2rem;
+`;
