c03f3850343f4f2cf07112a4328e87ff885b5935
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-09 06:00:23 +0900

    Feat: apply redux store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,26 @@
 import '../styles/reset.scss';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import AuthTokenProviders from '../contexts/AuthTokenProviders';
-import ResultsProviders from '../contexts/ResultsProviders';
-import ProfilesProviders from '../contexts/ProfilesProviders';
-import CommentsProviders from '../contexts/CommentsProviders';
-import CurrentPatientProviders from '../contexts/CurrentPatientProviders';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import rootReducer from '../modules/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from '../modules/index';
+
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+export const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(logger, sagaMiddleware)),
+);
+sagaMiddleware.run(rootSaga);
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<AuthTokenProviders>
-			<ResultsProviders>
-				<CurrentPatientProviders>
-					<ProfilesProviders>
-						<CommentsProviders>
-							<Component {...pageProps} />
-						</CommentsProviders>
-					</ProfilesProviders>
-				</CurrentPatientProviders>
-			</ResultsProviders>
-		</AuthTokenProviders>
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
 	);
 }
