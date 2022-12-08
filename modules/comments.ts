7242a4d2789d2c41fd59c37e27aea194ed0c7483
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-09 05:59:39 +0900

    Feat: comments reducer

---
+++ modules/comments.ts
@@ -0,0 +1,17 @@
+import { createAction, handleActions } from 'redux-actions';
+import { CommentsValueType } from '../@types/context';
+
+const SET_COMMENTS = 'results/SET_COMMENTS';
+
+export const setComments = createAction(SET_COMMENTS, (comments: CommentsValueType) => comments);
+
+const initialState: CommentsValueType = {};
+
+const comments = handleActions(
+	{
+		[SET_COMMENTS]: (state, action) => ({ ...action.payload }),
+	},
+	initialState,
+);
+
+export default comments;
