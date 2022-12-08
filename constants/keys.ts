4817902c24fdb18682d0fa7b7fbcaa0f08d02cb8
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-09 06:02:12 +0900

    Feat: keys as constants

---
+++ constants/keys.ts
@@ -0,0 +1,10 @@
+export default class Keys {
+	static CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+	static API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+	static DISCOVERY_DOC = process.env.NEXT_PUBLIC_DISCOVERY_DOC;
+	static SCOPES = process.env.NEXT_PUBLIC_SCOPES;
+
+	static RESULTS_FOLDER_ID = '1b0ZZDRDM9telQUAmEF7l9b5qxp611fnU';
+	static PROFILES_FOLDER_ID = '10MF1d0SY38KrErHAMDYji1ID-FiLM9Oe';
+	static COMMENTS_FOLDER_ID = '1XIhnw2E2ThkwWgZnww03HSnBTjyIdBaN';
+}
