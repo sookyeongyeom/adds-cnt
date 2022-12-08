67b2e60ebb7f9b5330130a0cce982c6992cda78c
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-09 05:59:25 +0900

    Feat: results reducer

---
+++ modules/results.ts
@@ -0,0 +1,17 @@
+import { createAction, handleActions } from 'redux-actions';
+import { ResultsValueType } from '../@types/context';
+
+const SET_RESULTS = 'results/SET_RESULTS';
+
+export const setResults = createAction(SET_RESULTS, (results: ResultsValueType) => results);
+
+const initialState: ResultsValueType = {};
+
+const results = handleActions(
+	{
+		[SET_RESULTS]: (state, action) => ({ ...action.payload }),
+	},
+	initialState,
+);
+
+export default results;
