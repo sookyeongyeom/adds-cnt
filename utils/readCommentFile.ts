03b6200e1ff952ba67c08ae37abe97297afc819e
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-09 23:53:21 +0900

    Feat: readCommentFile util

---
+++ utils/readCommentFile.ts
@@ -0,0 +1,35 @@
+import * as XLSX from 'xlsx';
+import { store } from '../pages/_app';
+import { CommentsValueType } from '../@types/context';
+import TestTypes from '../constants/tests';
+import Comments from '../models/Comments';
+import { setComments } from '../modules/comments';
+
+export default function readCommentFile(file: any) {
+	let reader = new FileReader();
+
+	reader.onload = function () {
+		const data = reader.result;
+		const workBook = XLSX.read(data, { type: 'binary' });
+
+		workBook.SheetNames.forEach((sheetName) => {
+			const rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
+			const commentsTemp: CommentsValueType = {};
+
+			rows.forEach((row: any) => {
+				const patientId = `${row.PatientID}`;
+				const cardSorting = row[TestTypes.cardSorting];
+				const wordColor = row[TestTypes.wordColor];
+				const trailMaking = row[TestTypes.trailMaking];
+				const total = row.Conclusion;
+
+				const comments = new Comments(cardSorting, wordColor, trailMaking, total);
+				commentsTemp[patientId] = comments;
+			});
+
+			store.dispatch(setComments(commentsTemp));
+		});
+	};
+
+	if (file !== null) reader.readAsBinaryString(file);
+}
