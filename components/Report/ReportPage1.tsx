2fa0dc5a041168ff5b8cc2254e8e5b9906aaf4f4
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-16 06:54:29 +0900

    Feat: reportPage1 profile table

--- components/Report/ReportPage1.tsx
+++ components/Report/ReportPage1.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 import Colors from '../../constants/colors';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../modules/index';
+import Fonts from '../../constants/fonts';
 
 export default function ReportPage1() {
+	const profiles = useSelector(({ profiles }: RootState) => profiles);
+	const focusId = useSelector(({ focusId }: RootState) => focusId);
+
 	return (
 		<S.Page1Container id='page1'>
 			<S.NavyBackground src='/assets/page1_남색배경.png' />
@@ -18,12 +24,38 @@ export default function ReportPage1() {
 				</h1>
 				<h2>(CNT 4.0) 전두엽 실행기능 검사</h2>
 			</S.Title>
+			<S.ProfileTable>
+				<tbody>
+					<tr>
+						<th>이 름</th>
+						<td>{profiles[focusId] && profiles[focusId].getName()}</td>
+					</tr>
+					<tr>
+						<th>학 교</th>
+						<td>{profiles[focusId] && profiles[focusId].getSchool()}</td>
+					</tr>
+					<tr>
+						<th>만 연 령</th>
+						<td>{profiles[focusId] && profiles[focusId].getAge() + '세'}</td>
+					</tr>
+					<tr>
+						<th>성 별</th>
+						<td>{profiles[focusId] && profiles[focusId].getSex()}</td>
+					</tr>
+					<tr>
+						<th>실 시 일</th>
+						<td>{profiles[focusId] && profiles[focusId].getDate()}</td>
+					</tr>
+				</tbody>
+			</S.ProfileTable>
 		</S.Page1Container>
 	);
 }
 
 namespace S {
-	export const Page1Container = styled.div``;
+	export const Page1Container = styled.div`
+		position: relative;
+	`;
 
 	export const Background = styled.img`
 		width: 100%;
@@ -52,8 +84,8 @@ namespace S {
 
 	export const YonseiLogo = styled(Background)`
 		top: unset;
-		bottom: 24rem;
-		left: 1rem;
+		bottom: 29rem;
+		left: 0rem;
 		width: 45%;
 	`;
 
@@ -89,5 +121,46 @@ namespace S {
 		width: 1rem;
 		height: 9rem;
 		top: 0.3rem;
+	`;
+
+	export const ProfileTable = styled.table`
+		position: absolute;
+		z-index: 10;
+		right: 2rem;
+		bottom: 5rem;
+
+		th,
+		td {
+			padding: 0.8rem;
+			position: relative;
+			top: 0.7rem;
+		}
+
+		th {
+			${Fonts.subtitle16semibold}
+			width: 16rem;
+			padding: 0.8rem 4rem;
+			padding-right: 5rem;
+			text-align: justify;
+
+			&:after {
+				content: '';
+				display: inline-block;
+				width: 100%;
+			}
+		}
+
+		td {
+			text-align: left;
+			width: 23rem;
+		}
+
+		tr:first-of-type {
+			th,
+			td {
+				border-top: 0.1rem solid ${Colors.gray500};
+				border-bottom: 0.1rem solid ${Colors.gray500};
+			}
+		}
 	`;
 }
