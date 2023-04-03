53f655559a56d018213617502d2145a6a534ca0f
sookyeongyeom <sookyeong.dev@gmail.com>
2023-04-03 13:44:44 +0900

    Feat: load fonts at document head instead of scss file

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,14 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head></Head>
+				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+					<link
+						rel='stylesheet'
+						href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
