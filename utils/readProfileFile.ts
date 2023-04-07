8229438f002b4c34fc3af21e08d342820313f80b
sookyeong <sookyeong.dev@gmail.com>
2023-04-07 15:25:17 +0900

    Feat: handle error when parse examine date (#3)

--- utils/readProfileFile.ts
+++ utils/readProfileFile.ts
@@ -17,17 +17,33 @@ export default function readProfileFile(file: any) {
 			const rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
 			const profilesTemp: ProfilesValueType = {};
 
-			rows.forEach((row: any) => {
+			rows.forEach((row: any, i) => {
 				const patientId = row.PatientID;
 				const name = row.이름;
 				const birthDate = excelSerialDateToJSDate(row.생년월일);
 				const age = calculateAge(birthDate);
 				const sex = row.성별;
 				const school = row.학교;
-				const [month, day, year] = excelSerialDateToJSDate(row.실시일)
-					.toLocaleDateString()
-					.split('/');
-				const date = year + '-' + month.padStart(2, '0') + '-' + day.padStart(2, '0');
+				// const [month, day, year] = excelSerialDateToJSDate(row.실시일)
+				// 	.toLocaleDateString()
+				// 	.split('/');
+				// if (month && day && year)
+				// 	date = year + '-' + month.padStart(2, '0') + '-' + day.padStart(2, '0');
+
+				let date = '';
+				try {
+					date = excelSerialDateToJSDate(row.실시일).toLocaleDateString('ko-KR').slice(0, -1);
+				} catch (e) {
+					console.log(e);
+					date = 'Parse Failed';
+				}
+
+				if (!i) {
+					console.log(row);
+					console.log(row.실시일);
+					console.log(excelSerialDateToJSDate(row.실시일));
+					console.log(date);
+				}
 
 				const profiles = new Profiles(name, age, sex, school, date);
 				console.log(profiles);
